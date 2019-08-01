418ad9df421b3857f79d832c9749ee8bb4840361
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-08-01 18:44:19 +0300

    random commit message

---
+++ SocialGoal/Iterator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialGoal
+{
+    public class Iterator
+    {
+        private List<string> _list = new List<string>();
+        public void Execute(int amount)
+        {
+            _list.Add("This is the first string to add.");
+
+            for (var i = 0; i < amount; i++)
+            {
+                _list.Add($"This is item {i+1}/{amount} with id {Guid.NewGuid()}.");
+            }
+
+            _list.Add("This is the last string to add.");
+        }
+    }
+}
